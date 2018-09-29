d63c90f818e5dbccf0a1731adcc20a32bc4765f0
彭涧 <jamesjianpeng@gmail.com>
2018-09-29 15:16:49 +0800

    build(webpack/ 添加 cdn 方式加载部分三方库)

--- webpack/webpack.ts.vue.conf.js
+++ webpack/webpack.ts.vue.conf.js
@@ -17,13 +17,18 @@ module.exports = {
         filename: '[name].js'
     },
     resolve: {
-        alias: {
-           /**
-            * @todo https://cn.vuejs.org/v2/guide/installation.html#%E8%BF%90%E8%A1%8C%E6%97%B6-%E7%BC%96%E8%AF%91%E5%99%A8-vs-%E5%8F%AA%E5%8C%85%E5%90%AB%E8%BF%90%E8%A1%8C%E6%97%B6
-            * 需要编译
-            */
-           'vue$': 'vue/dist/vue.esm.js'
-        }
+        // alias: {
+        //    /**
+        //     * @todo https://cn.vuejs.org/v2/guide/installation.html#%E8%BF%90%E8%A1%8C%E6%97%B6-%E7%BC%96%E8%AF%91%E5%99%A8-vs-%E5%8F%AA%E5%8C%85%E5%90%AB%E8%BF%90%E8%A1%8C%E6%97%B6
+        //     * 需要编译
+        //     */
+        //    'vue$': 'vue/dist/vue.esm.js'
+        // }
+        // extensions: [ '.ts', '.vue', '.json' ] // extensions 的数组长度大于等于 3
+    },
+    externals: {
+        vue: 'Vue', // 需要知道库中暴露出来的 name，key 是我们在项目 import Vue from 'vue', value 就是 资源中暴露出来的名字
+        'vue-router': 'VueRouter'
     },
     module: {
         rules: [
@@ -52,7 +57,7 @@ module.exports = {
     plugins: [
         new VueLoaderPlugin(),
         new HtmlWebpackPlugin({
-            title: 'TypeScript and Threejs project',
+            template: path.resolve(__dirname, '../src/app-ts-vue/index-ts-vue.html'),
             meta: {
                 viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
             }
