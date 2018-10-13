e3ae6bb1b963760cc615c382dc423742242c060c
彭涧 <jamesjianpeng@gmail.com>
2018-10-13 17:02:43 +0800

    feat: add dynamicShellSort

--- src/app/sort/helper/shell-sort.js
+++ src/app/sort/helper/shell-sort.js
@@ -1,11 +1,10 @@
-
-const interval = [5, 3, 1];
-
 /**
- * 希尔排序
+ * 固定间隔的希尔排序
  * @function
  * @returns {Object} {time}
  */
+const interval = [5, 3, 1];
+
 const shellSort = function () {
     var list = this;
 
@@ -21,9 +20,6 @@ const shellSort = function () {
     /** @type {number} 临时储存固定对比的元素 */
     var temp;
 
-    /** @type {number} 根据间隔动态计算的索引 */
-    var intervalD;
-
     /** @type {number} 最深层的计数器 */
     var count = 1;
 
@@ -34,7 +30,7 @@ const shellSort = function () {
 
     intervalIndex = 0;
     while (intervalIndex < interval.length) {
-        /** 获得对比元素之间的间隔： interval[intervalIndex]，摒弃指定的间隔初始化起点：outer */
+        /** 获得对比元素之间的间隔： interval[intervalIndex]，并指定的间隔初始化起点：outer */
         outer = interval[intervalIndex];
         while (outer < list.length) {
             /** 缓存一次对比的元素 */
@@ -42,8 +38,8 @@ const shellSort = function () {
 
             /** 第二层循环索引的初始化 */
             inner = outer;
-            intervalD = inner - interval[intervalIndex];
-            while (inner >= interval[intervalIndex] && list[intervalD] > temp) {
+
+            while (inner >= interval[intervalIndex] && /** 关键点：inner - dynamicInterval */ list[inner - interval[intervalIndex]] > temp) {
                 /** 以指定的间隔循环数组 */
                 list[inner] = list[inner - interval[intervalIndex]];
                 inner -= interval[intervalIndex];
@@ -60,8 +56,81 @@ const shellSort = function () {
 
     return {
         time,
-        count
+        count,
+        list
     };
 };
 
-export { shellSort };
+/**
+ * 动态间隔的希尔排序
+ * @function
+ * @returns {Object} {time}
+ */
+
+const dynamicShellSort = function () {
+    var list = this;
+
+    /** @type {number} 数组的长度 */
+    var len = list.length;
+
+    /** @type {number} 动态的间隔 */
+    var dynamicInterval;
+
+    /** @type {number} 第一层循环的索引 */
+    var outer;
+
+    /** @type {number} 第二层循环的索引 */
+    var inner;
+
+    /** @type {number} 临时储存固定对比的元素 */
+    var temp;
+
+    /** @type {number} 最深层的计数器 */
+    var count = 1;
+
+    /** @type {number} 函数执行时间 */
+    var time = new Date().getTime();
+
+    if (Object.prototype.toString.call(list) !== '[object Array]') return new TypeError('list not array');
+
+    dynamicInterval = 1;
+    
+    /** #region 动态生成间隔 */
+    while (dynamicInterval < (len / 3)) {
+        dynamicInterval = len * 3 + 1;
+    }
+    /** #endregion 动态生成间隔 */
+
+    while (dynamicInterval >= 1) {
+        /** 并指定的间隔初始化起点：outer */
+        outer = dynamicInterval;
+
+        while (outer < len) {
+            /** 缓存一次对比的元素 */
+            temp = list[outer];
+
+            /** 第二层循环索引的初始化 */
+            inner = outer;
+            while (inner >= dynamicInterval && /** 关键点 1：inner - dynamicInterval */ list[inner - dynamicInterval] > temp) {
+                /** 以指定的间隔循环数组 */
+                list[inner] = list[inner - dynamicInterval];
+                inner -= dynamicInterval;
+                count++;
+            }
+            list[inner] = temp;
+            outer++;
+        }
+
+        dynamicInterval = Math.ceil((dynamicInterval - 1) / 3); /** 关键点 2: 必须使用向上取整，使用 parseInt 和 Math.floor 之后排序不准确 */
+    }
+
+    time = new Date().getTime() - time;
+
+    return {
+        time,
+        count,
+        list
+    };
+};
+
+export { shellSort, dynamicShellSort };
